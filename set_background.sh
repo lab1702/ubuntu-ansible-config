e0c5b4b87b7dcca08bccfe077c3e736dfab563f5
lab1702 <lab1702@gmail.com>
2020-12-17 19:44:32 -0500

    Add files via upload

---
+++ set_background.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+gsettings set org.gnome.desktop.background primary-color '#000000'
+gsettings set org.gnome.desktop.background secondary-color '#000000'
+gsettings set org.gnome.desktop.background color-shading-type 'solid'
+gsettings set org.gnome.desktop.background picture-options 'none'
+gsettings set org.gnome.desktop.background picture-uri ''
